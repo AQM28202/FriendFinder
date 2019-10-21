5b8d4d17e517d338ac3d79f9e74797be69aa8d09
Anthony McPherson <aqm28202@gmail.com>
2019-10-21 11:55:09 -0400

    updated scores in freinds array

--- app/data/friend.js
+++ app/data/friend.js
@@ -2,32 +2,32 @@ var friendsList = [
     {
         name: 'Oprah Winfrey',
         image: 'https://media.giphy.com/media/11HxElzKUMmBxu/giphy.gif',
-        scores: [1, 4, 3, 3, 5, 2, 4, 4, 5, 2]
+        scores: [1, 2, 4, 3, 4, 5, 2, 1, 5, 3]
     },
     {
         name: 'Tyler Perry',
         image: 'https://media.giphy.com/media/LfWF1M5AdDXEc/giphy.gif',
-        scores: [1, 3, 4, 5, 2, 2, 3, 3, 3, 4]
+        scores: [1, 2, 5, 3, 4, 5, 1, 3, 5, 2]
     },
     {
         name: 'Michael Jordan',
         image: 'https://media.giphy.com/media/O5NyCibf93upy/giphy.gif',
-        scores: [2, 3, 5, 5, 5, 4, 3, 3, 1, 2]
+        scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
     },
     {
         name: 'Martin Luther King, Jr.',
         image: 'https://media.giphy.com/media/cMKJb3fQZ4rpm/giphy.gif',
-        scores: [4, 4, 4, 4, 3, 1, 2, 2, 3, 1]
+        scores: [4, 3, 3, 3, 4, 3, 2, 1, 5, 3]
     },
     {
         name: 'Malcom X',
         image: 'https://media.giphy.com/media/3pPqL4GQyS8yQ/giphy.gif',
-        scores: [3, 3, 4, 2, 1, 5, 2, 1, 1, 3]
+        scores: [3, 2, 5, 2, 4, 2, 3, 4, 2, 4]	
     },
     {
         name: 'Barack Obama',
         image: 'https://media.giphy.com/media/pPhyAv5t9V8djyRFJH/giphy.gif',
-        scores: [1, 2, 3, 1, 2, 3, 1, 5, 5, 5]
+        scores: [5, 5, 1, 3, 1, 5, 1, 5, 5, 3]
     }
 
 ]
