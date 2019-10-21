3af515c3ae49676c84b2645159f612db74965c43
Anthony McPherson <aqm28202@gmail.com>
2019-10-21 11:52:20 -0400

    added friends to array for friends.js

---
+++ app/data/friend.js
@@ -0,0 +1,35 @@
+var friendsList = [                   
+    {
+        name: 'Oprah Winfrey',
+        image: 'https://media.giphy.com/media/11HxElzKUMmBxu/giphy.gif',
+        scores: [1, 4, 3, 3, 5, 2, 4, 4, 5, 2]
+    },
+    {
+        name: 'Tyler Perry',
+        image: 'https://media.giphy.com/media/LfWF1M5AdDXEc/giphy.gif',
+        scores: [1, 3, 4, 5, 2, 2, 3, 3, 3, 4]
+    },
+    {
+        name: 'Michael Jordan',
+        image: 'https://media.giphy.com/media/O5NyCibf93upy/giphy.gif',
+        scores: [2, 3, 5, 5, 5, 4, 3, 3, 1, 2]
+    },
+    {
+        name: 'Martin Luther King, Jr.',
+        image: 'https://media.giphy.com/media/cMKJb3fQZ4rpm/giphy.gif',
+        scores: [4, 4, 4, 4, 3, 1, 2, 2, 3, 1]
+    },
+    {
+        name: 'Malcom X',
+        image: 'https://media.giphy.com/media/3pPqL4GQyS8yQ/giphy.gif',
+        scores: [3, 3, 4, 2, 1, 5, 2, 1, 1, 3]
+    },
+    {
+        name: 'Barack Obama',
+        image: 'https://media.giphy.com/media/pPhyAv5t9V8djyRFJH/giphy.gif',
+        scores: [1, 2, 3, 1, 2, 3, 1, 5, 5, 5]
+    }
+
+]
+
+module.exports = friendsList
