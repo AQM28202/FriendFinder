868e32a9093ad6610ab53ccd1567a410b24b0d44
Anthony McPherson <aqm28202@gmail.com>
2019-10-21 17:28:43 -0400

    adding and deleting comments

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,35 +1,35 @@
 var friends = [                   
     {
         name: 'Oprah Winfrey',
-        image: 'https://media.giphy.com/media/11HxElzKUMmBxu/giphy.gif',
+        photo: 'https://media.giphy.com/media/11HxElzKUMmBxu/giphy.gif',
         scores: [1, 2, 4, 3, 4, 5, 2, 1, 5, 3]
     },
     {
         name: 'Tyler Perry',
-        image: 'https://media.giphy.com/media/LfWF1M5AdDXEc/giphy.gif',
+        photo: 'https://media.giphy.com/media/LfWF1M5AdDXEc/giphy.gif',
         scores: [1, 2, 5, 3, 4, 5, 1, 3, 5, 2]
     },
     {
         name: 'Michael Jordan',
-        image: 'https://media.giphy.com/media/O5NyCibf93upy/giphy.gif',
+        photo: 'https://media.giphy.com/media/O5NyCibf93upy/giphy.gif',
         scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
     },
     {
         name: 'Martin Luther King, Jr.',
-        image: 'https://media.giphy.com/media/cMKJb3fQZ4rpm/giphy.gif',
+        photo: 'https://media.giphy.com/media/cMKJb3fQZ4rpm/giphy.gif',
         scores: [4, 3, 3, 3, 4, 3, 2, 1, 5, 3]
     },
     {
         name: 'Malcom X',
-        image: 'https://media.giphy.com/media/3pPqL4GQyS8yQ/giphy.gif',
+        photo: 'https://media.giphy.com/media/3pPqL4GQyS8yQ/giphy.gif',
         scores: [3, 2, 5, 2, 4, 2, 3, 4, 2, 4]	
     },
     {
         name: 'Barack Obama',
-        image: 'https://media.giphy.com/media/pPhyAv5t9V8djyRFJH/giphy.gif',
+        photo: 'https://media.giphy.com/media/pPhyAv5t9V8djyRFJH/giphy.gif',
         scores: [5, 5, 1, 3, 1, 5, 1, 5, 5, 3]
     }
 
 ]
 
-module.exports = friends
+module.exports = friends;
