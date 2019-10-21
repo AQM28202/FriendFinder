8cbca6387f66a107fa990d54468fc70dd33ac203
Anthony McPherson <aqm28202@gmail.com>
2019-10-21 11:22:30 -0400

    boiler plate server setup

---
+++ server.js
@@ -0,0 +1,26 @@
+// Dependencies
+var express = require("express");
+var path = require("path");
+
+
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsinG
+app.use(express.urlencoded({ extended: true }));  
+app.use(express.json());
+app.use(express.static('./app/public'));
+
+
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+
+require("./app/routing/api_routes.js")(app);
+require("./app/routing/html_routes.js")(app);
+
+// Starts the server to begin listening
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT);
+});
